a7334e76d62feefdf3fdc19547ee005071ea8833
Ramom Lopes <ramoncezar531@gmail.com>
2024-01-01 19:45:07 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -6,6 +6,7 @@ import chess.ChessPiece;
 public class UI {
 
     public static void printBoard(ChessPiece[][] pieces){
+        System.out.println();
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++) {
@@ -13,7 +14,7 @@ public class UI {
             }
             System.out.println();
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println("  A B C D E F G H");
     }
 
     private static void printPiece(ChessPiece piece){
